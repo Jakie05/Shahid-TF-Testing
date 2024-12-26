e003a64d64b91f8d67fd1e6ce8ea129d7ecb5c39
Jakie Singh <88844922+Jakie05@users.noreply.github.com>
2024-12-26 13:16:04 +0530

    Update main.tf

--- testing/main.tf
+++ testing/main.tf
@@ -1,24 +1,36 @@
 module "terraform-azurerm-kubernetesservices" {
-  source                 = "../modules/aks/"
-  name                   = "IBCITEST3MYAKS01"
-  dns_prefix             = "exampleaks1"
-  private_cluster_enabled = true
-  local_account_disabled  = true
-  kubernetes_version      = "1.29.8"
-  sku_tier                = "Standard"
+  source                        = "../modules/aks/"
+  name                          = "IBCITEST3MYAKS01"
+  dns_prefix                    = "exampleaks1"
+  private_cluster_enabled       = true
+  local_account_disabled        = true
+  kubernetes_version            = "1.29.8"
+  sku_tier                      = "Standard"
   public_network_access_enabled = false
   automatic_upgrade_channel     = "stable"
-  disk_encryption_set_id        = "/subscriptions/ac6361f8-4743-4781-946b-fe85b704d358/resourceGroups/ibci-uat-tfe-rg01/providers/Microsoft.Compute/diskEncryptionSets/IBCI-UAT-TFE-RG01-DiskEncryptionSet"
+  azure_policy_enabled          = true
+  node_os_upgrade_channel       = "NodeImage"
+  support_plan                  = "KubernetesOfficial"
+  key_vault_network_access      = "Public"
   private_cluster_public_fqdn_enabled = false
-  secret_rotation_enabled  = true 
-  secret_rotation_interval = "2m"
+  secret_rotation_enabled        = true 
+  secret_rotation_interval       = "2m"
+
+  storage_profile = {
+    blob_driver_enabled = true
+    disk_driver_enabled = true
+    file_driver_enabled = true
+  }
+
   network_plugin           = "azure"
   network_policy           = "calico"
   dns_service_ip           = "10.152.0.10"
   outbound_type            = "userDefinedRouting"
   service_cidr             = "10.152.0.0/17"
+  pod_cidrs                = 
+  network_plugin_mode      = 
+
   azure_rbac_enabled       = true
-  tenant_id                  = "30fe8ff1-adc6-444d-ba94-1238894df42c"  
   identity_type              = "UserAssigned"        
   httpproxy                  = "http://10.227.3.206:80/"
   httpsproxy                 = "http://10.227.3.206:80/"
