be5b236ac03f8815397f01d4436e5cf0473f6590
Jakie Singh <88844922+Jakie05@users.noreply.github.com>
2024-12-26 13:39:33 +0530

    Update output.tf

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -1,11 +1,11 @@
 output "kubelet_identity_obj_id" {
-    value = azurerm_kubernetes_cluster.azure_kubernetes_cluster.kubelet_identity[0].object_id
+    value = azurerm_kubernetes_cluster.azure_kubernetes_cluster[*].kubelet_identity[0].object_id
 }
 
 output "cluster_id" {
-    value = azurerm_kubernetes_cluster.azure_kubernetes_cluster.id
+    value = azurerm_kubernetes_cluster.azure_kubernetes_cluster[*].id
 }
 
 output "cluster_name" {
-    value = azurerm_kubernetes_cluster.azure_kubernetes_cluster.name
+    value = azurerm_kubernetes_cluster.azure_kubernetes_cluster[*].name
 }
