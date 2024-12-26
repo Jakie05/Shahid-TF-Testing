2a6c8cf9aaab5e7d5712c4b59e0a217a23493dc2
Jakie Singh <88844922+Jakie05@users.noreply.github.com>
2024-12-26 12:27:35 +0530

    Update provider.tf

--- testing/provider.tf
+++ testing/provider.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  client_id       = var.client_id     
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id    
+  tenant_id       = var.tenant_id   
+}
